6032b387b12d51ce8dd3bf8c0f77e31397771566
sgc88 <sehribancheng@yahoo.com>
2018-05-27 11:31:46 -0700

    added starts to count performance as game in play

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Create a list that holds all of your cards
-var cards=[
+const cards=[
   {
     rank: "udacity1",
     cardImage: "img/udacity.jpeg",
@@ -114,7 +114,7 @@ var cards=[
 ];
 
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -126,12 +126,22 @@ function shuffle(array) {
     return array;
 }
 
-var number = 0;
-var timeInSeconds = 0;
-var gameTime;
+let number = 0;
+let timeInSeconds = 0;
+let gameTime;
+let stars = ['*', '*', '*'];
+
+// function to convert stars in stars array into a string, to use in html.
+const starString = function(arr) {
+  let str = '';
+  for(let i=0; i < arr.length; i++) {
+    str += arr[i];
+  }
+  return str;
+}
 
 // counting clicks while game in play
-var mouseClicked = function() {
+const mouseClicked = function() {
   number++;
   if(number === 1) {
     //counting time while game in play
@@ -141,22 +151,37 @@ var mouseClicked = function() {
   }
   document.getElementById("moves").innerHTML= "Total Moves: " + number;
 
+  if(number === 5) {
+    stars.pop();
+    console.log("decremented stars: ", stars);
+    const newStars = starString(stars);
+    document.getElementById("stars").innerHTML = newStars;
+  }
+
+  if(number === 10) {
+    stars.pop();
+    console.log("decremented stars: ", stars);
+    const newStars = starString(stars);
+    document.getElementById("stars").innerHTML = newStars;
+  }
+
+
   // return number;
 }
 
-var cardsInPlay = [];
-var gameScore = 0;
-var gameSwitch = true;
+let cardsInPlay = [];
+let gameScore = 0;
+let gameSwitch = true;
 
 
  //set up the event listener for a card
-var flipCard = function(){
+const flipCard = function(){
   // we need to make sure we can't flip more then 2 cards to compare
   if(!gameSwitch) {
       console.log("You cant play");
       return;
     }
-     var cardId = this.getAttribute("data-id");
+     const cardId = this.getAttribute("data-id");
      if(cards[cardId].clicked === true) {
        console.log("already clicked");
        return;
@@ -188,37 +213,45 @@ var flipCard = function(){
   }
 
 
-  mouseClicked();
-
-//when the user wins the game
-  if(gameScore === 3){
+mouseClicked();
+  //when the user wins the game
+  if(gameScore === 8){
     clearInterval(gameTime);
     setTimeout(function() {
       alert("You won the game with total of " + number +  " moves, in " + timeInSeconds + " seconds!");
+      if(stars.length === 3) {
+        alert("You did great! Your performance was " + stars.length + " stars!");
+      }
+      if(stars.length === 2) {
+        alert("You did good! Your performance was " + stars.length + " stars!");
+      }
+      if(stars.length === 1) {
+        alert("You did OK. Your performance was " + stars.length + " star, try to solve the memory game faster next time.");
+      }
       cardsInPlay = [];
       gameScore = 0;
       timeInSeconds = 0;
+      stars = ['*', '*', '*'];
+      newStars = starString(stars);
+      document.getElementById("stars").innerHTML = newStars;
       document.getElementById("trackScore").innerHTML = 0;
       document.getElementById("moves").innerHTML = "Total moves: " + 0;
 
       resetGame();
     }, 750);
   }
-
 };
 
 
 
-var checkForMatch = function(){
+const checkForMatch = function(){
   //if the list already has another card check to see if two cards match
   if(cardsInPlay.length <= 2){
       	if(cardsInPlay[0].rank === cardsInPlay[1].rank){
       		alert("you found a match!");
       		gameScore ++;
       		document.getElementById("trackScore").innerHTML= gameScore;
-
           cardsInPlay = [];
-
         }else{
           //if they dont match remove both cards from array and hide the symbols
           setTimeout(function() {
@@ -232,16 +265,15 @@ var checkForMatch = function(){
 
       }
   }
-
 }
 
- var createBoard= function(){
+ const createBoard = function(){
    //shuffle the list of cards using the provided "shuffle" method above
    //we need to shuffle the cards each time we create the new board
 	 shuffle(cards);
    //loop through each card, create its HTML and add each card's HTML to the page
-	for(var i=0; i < cards.length; i++){
-		var cardElement= document.createElement("img");
+	for(let i=0; i < cards.length; i++){
+		const cardElement= document.createElement("img");
 		cardElement.setAttribute("src", "img/back.png");
 		cardElement.setAttribute("data-id", i);
 		cardElement.addEventListener("click", flipCard);
@@ -253,25 +285,25 @@ var checkForMatch = function(){
 createBoard();
 
 
-var resetGame = function(){
+const resetGame = function(){
   clearInterval(gameTime);
   timeInSeconds = 0;
 	// delete cards in cardsInPlay array
-	for(i = cardsInPlay.length; i> 0; i--){
+	for(let i = cardsInPlay.length; i> 0; i--){
 		cardsInPlay.pop();
 	}
 
 	// get rid of the current board
-	var meset = document.getElementById("game-board");
+	const meset = document.getElementById("game-board");
 	meset.remove();
 
 	// because we removed the div "game-board" we need to recreate it so the createBoard() can be executed.
-	var newD = document.createElement("div");
+	const newD = document.createElement("div");
 	newD.setAttribute("id", "game-board");
 	newD.setAttribute("class", "board clearfix");
 	document.getElementById("mainTag").appendChild(newD);
 
-  for(var i=0; i < cards.length; i++ ){
+  for(let i=0; i < cards.length; i++ ){
     cards[i].clicked = false;
   }
 
@@ -280,6 +312,9 @@ var resetGame = function(){
 
   gameScore = 0;
   number = 0;
+  stars = ['*', '*', '*'];
+  newStars = starString(stars);
+  document.getElementById("stars").innerHTML = newStars;
   document.getElementById("trackScore").innerHTML = gameScore;
   document.getElementById("moves").innerHTML = "Total moves: " + number;
 };
